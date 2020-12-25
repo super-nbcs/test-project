a8be753215f1236993762e423daaf3ace46cf8ef
snow <snow@huigouiot.com>
2020-12-25 17:41:46 +0800

    本地仓库

---
+++ git01/src/com/huigou/controller/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.huigou.controller;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hahahahah");
+        System.out.println("HelloWorld!!!");
+    }
+
+}
